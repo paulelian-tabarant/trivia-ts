33010d500b6b4d724a8f2306ea28998f80e203ef
Paul-Elian Tabarant <paul-elian.tabarant@octo.com>
2026-02-22 20:33:39 +0100

    refactor: indents

--- src/game-runner.ts
+++ src/game-runner.ts
@@ -11,13 +11,13 @@ export class GameRunner {
         do {
 
             game.roll(Math.floor(Math.random() * 6) + 1);
-        
+
             if (Math.floor(Math.random() * 10) == 7) {
-            notAWinner = game.wrongAnswer();
+                notAWinner = game.wrongAnswer();
             } else {
-            notAWinner = game.wasCorrectlyAnswered();
+                notAWinner = game.wasCorrectlyAnswered();
             }
-        
+
         } while (notAWinner);
     }
 }
