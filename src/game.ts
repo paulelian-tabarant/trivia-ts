046d5c2dee6278ece53f4f0d3c1ae7adf02c8a02
Paul-Elian Tabarant <paul-elian.tabarant@octo.com>
2026-02-25 15:48:43 +0100

    refactor

--- src/game.ts
+++ src/game.ts
@@ -101,12 +101,15 @@ export class Game implements Logger {
             0: QuestionCategory.POP,
             1: QuestionCategory.SCIENCE,
             2: QuestionCategory.SPORTS,
+            3: QuestionCategory.ROCK,
             4: QuestionCategory.POP,
             5: QuestionCategory.SCIENCE,
             6: QuestionCategory.SPORTS,
+            7: QuestionCategory.ROCK,
             8: QuestionCategory.POP,
             9: QuestionCategory.SCIENCE,
             10: QuestionCategory.SPORTS,
+            11: QuestionCategory.ROCK,
         }
 
         if (CATEGORY_BY_PLACE[this.places[this.currentPlayer]]) {
