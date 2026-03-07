5c68948e25401da9869a7c6771448bf7201007ea
Paul-Elian Tabarant <paul.tabarant@gmail.com>
2026-03-07 17:47:18 +0100

    docs: indicate buggy implementation

--- src/player.ts
+++ src/player.ts
@@ -5,10 +5,11 @@ export class Player {
     private _location: number;
     private _numberOfGoldCoins: number;
     private isInPenaltyBox: boolean = false;
-    private lastRoll: number;
+    private lastRoll: number | undefined = undefined;
 
     private static readonly NUMBER_OF_GOLD_COINS_TO_WIN = 6;
 
+    // FIXME: two last parameters are to override the initial value with NaN, but they are very likely to be caused by a bug
     constructor(name: string, number: number, numberOfGoldCoins: number = 0, location: number = 0) {
         this.name = name;
         this.number = number;
